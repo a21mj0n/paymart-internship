a649979ba38d284c057b6b51c72b2da6025f0801
Aziz <alienhunter@bk.ru>
2021-11-29 10:51:49 +0500

    a little changes

---
+++ vue-own/src/components/products.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="products">
+    <div class="product">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang='scss'>
+  .products{
+    display: flex;
+    align-items: center;
+    .product{
+      width: 33.3%;
+    }
+  }
+</style>
