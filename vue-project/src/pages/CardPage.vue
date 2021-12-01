f127c82c9014da5879263d02203ea430792a73e9
azizbek <azizbekkarimjonov12@gmail.com>
2021-12-01 13:39:20 +0500

    store cart added

--- vue-project/src/pages/CardPage.vue
+++ vue-project/src/pages/CardPage.vue
@@ -1,17 +1,34 @@
 <template>
     <div>
+        <!-- from store -->
+        <h1>{{ this.$store.state.cart.products }}</h1>
+        <h1>{{ findByAge ()}}</h1>
+
+        <!-- with getters -->
+
+        <h1>{{ this.$store.getters['cart/getProduct'](20)}} </h1>
+        
         <p>
             <strong>old verison:</strong>You opened card by id {{$route.params.id}}
         </p>
+
         <p>
             <strong>simple verison:</strong>You opened card by id {{ id }}
         </p>
+
     </div>
 </template>
+
 <script>
+
 export default {
     name: 'CardPage',
-    props: ['id']
+    props: ['id'],
+    methods:{
+        findByAge(){
+            return  this.$store.state.cart.products.find(({age}) => age == 20)
+        }
+    }
 }
 </script>
 <style scoped>
