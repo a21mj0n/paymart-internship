08750c693ec865405f3fddb84dfb70a13a98f65c
azizbek <azizbekkarimjonov12@gmail.com>
2021-12-02 10:54:34 +0500

    templated created-2

---
+++ admin-panel/src/pages/AdminPage.vue
@@ -0,0 +1,16 @@
+<template >
+    <div class="wrapper">
+        123
+        <i class="fa fa-bath"></i>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    .fa{
+        color: black;
+    }
+</style>
