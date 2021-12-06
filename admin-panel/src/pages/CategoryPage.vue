90bb57a72cfcc27117c3bedb5193b5025fe8a399
azizbek <azizbekkarimjonov12@gmail.com>
2021-12-06 11:13:39 +0500

    Layout add

---
+++ admin-panel/src/pages/CategoryPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <my-content/>
+    </div>
+</template>
+<script>
+import MyContent from '../components/admin-panel/MyContent.vue'
+export default {
+  components: { MyContent },
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- admin-panel/src/router/routes.js
+++ admin-panel/src/router/routes.js
@@ -3,11 +3,22 @@ export default [
     {
         path: '/admin',
         name: 'admin',
+        meta: {
+            layout: "admin"
+        },
         component: () => import('../pages/AdminPage')
     },
     {
         path: '/auth',
         name: "auth",
         component:  () => import('../pages/AuthPage')
-    }
+    },
+    {
+        path: '/admin/category',
+        name: "category",
+        meta: {
+            layout: "admin"
+        },
+        component:  () => import('../pages/CategoryPage')
+    },
 ]
