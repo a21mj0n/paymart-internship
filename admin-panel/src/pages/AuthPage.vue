87e7a4a93f17b4a7ba6e0d4a47350f2c06caf62a
azizbek <azizbekkarimjonov12@gmail.com>
2021-12-09 11:58:13 +0500

    auth done

--- admin-panel/src/pages/AuthPage.vue
+++ admin-panel/src/pages/AuthPage.vue
@@ -1,110 +1,209 @@
 <template>
   <div class="wrapper">
-    <div class="login">
-      <div class="login_block">
-        <form id="" action="#" class="feed-form" v-if="isLogin">
+    <div class="content" v-if="isLogin">
+      <div class="content_block">
+        <h3>Login</h3>
+        <p class="error" v-if="error">{{error}}</p>
+        <form id="" action="#" class="feed-form" @submit.prevent="loginUser">
           <div class="form_inputs">
-            <input name="name" required placeholder="Username" type="text" />
             <i class="fa fa-user" aria-hidden="true"></i>
+            <input 
+              name="name" 
+              required 
+              placeholder="Username" 
+              type="text" 
+              v-model="username"
+            />
           </div>
           <div class="form_inputs">
+            <i class="fa fa-lock" aria-hidden="true"></i>
             <input
               name="password"
-              required
               placeholder="password"
               type="password"
+              required
+              v-model="password"
             />
-            <i class="fa fa-lock" aria-hidden="true"></i>
           </div>
             <div class="form__block">
             <button class="form__block_btn">Login</button>
-            <a href="#" class="form__block_link"
-                ><p class="form_block_text">Forgot password?</p></a
-            >
+              <a href="#" class="form__block_link">
+                <p class="form_block_text">Forgot password?</p>
+              </a>
+            <a href="#" class="form__block_link" @click="isLogin = !isLogin">Sing Up</a>
             </div>
         </form>
       </div>
     </div>
 
-    <div class="register">
-        
+    <div class="content" v-else>
+       <div class="content_block">
+        <h3>Registration</h3>
+        <form id="" action="#" class="feed-form register-form" @submit.prevent="registerUser">
+          <div class="form_inputs">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <input name="username" required placeholder="Username" type="text"  v-model="username"/>
+          </div>
+          <div class="form_inputs">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <input name="first_name" required placeholder="First name" type="text"  v-model="first_name"/>
+          </div>
+          <div class="form_inputs">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <input name="last_name" required placeholder="Last name" type="text"  v-model="last_name"/>
+          </div>
+          <div class="form_inputs">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <input name="email" required placeholder="Email" type="email"  v-model="email"/>
+          </div>
+          <div class="form_inputs">
+            <i class="fa fa-lock" aria-hidden="true"></i>
+            <input
+              name="password"
+              required
+              placeholder="password"
+              type="password" v-model="password"
+            />
+          </div>
+          <div class="form_inputs">
+            <i class="fa fa-lock" aria-hidden="true"></i>
+            <input
+              name="password_confirmation"
+              required
+              placeholder="confirm password"
+              type="password"
+              v-model="password_confirmation"
+            />
+          </div>
+          <div class="form__block">
+            <button class="form__block_btn">Sign Up</button>
+            <a href="#" class="form__block_link">
+              <p class="form_block_text" @click="isLogin = !isLogin">Login</p>
+            </a>
+          </div>
+        </form>
+      </div> 
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "LoginPage",
   data(){
       return{
-          isLogin: true
+        isLogin: true,
+        username: "",
+        email: "",
+        first_name: "",
+        last_name: "",
+        password: "",
+        password_confirmation: "",
+        error: ""
       }
   },
-  created(){
-    console.log(this.$route);
+  methods: {
+
+    async registerUser(){
+      const user = {
+        username: this.username,
+        email: this.email,
+        first_name: this.first_name,
+        last_name: this.last_name,
+        password: this.password,
+        password_confirmation: this.password_confirmation
+      }
+      try{
+         const resp = await axios.post('https://marketpaymart.herokuapp.com/api/register', user)
+        if(resp.data.access_token){
+          localStorage.setItem('token', resp.data.access_token)
+          this.$router.push({name: "admin"})
+        }
+      }catch(e){
+        console.log(e);
+      }
+    },
+
+    async loginUser(){
+      try{
+        const user = {
+          username: this.username,
+          password: this.password,
+        }
+        const resp = await axios.post('https://marketpaymart.herokuapp.com/api/login', user)
+        if(resp.data.access_token){
+          localStorage.setItem('token', resp.data.access_token)
+          this.$router.push({name: "admin"})
+        }
+      }catch(err){
+        this.error= err.response.data.message
+        console.log(err);
+      }
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+a{
+  margin: 0;
+  padding: 0;
+}
+.error{
+  padding: 5px 0 ;
+  color: red;
+}
 .wrapper{
     width: 100%;
     height: 100vh;
+    background-color: rgb(235, 235, 235);
 }
-.login {
-  text-align: center;
-  min-height: 100vh;
-  padding: 350px 0 150px 0;
-  background-image: url("../assets/login_bg.jpg");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  height: 720px;
-  &_block {
-    margin: 0 auto;
-    width: 300px;
-    min-height: 238px;
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h3{
+    color: red;
+    padding: 10px 0;
   }
   form {
     .form_inputs {
       position: relative;
       input {
-        background-color: #c02147;
+        background-color: transparent;
         height: 45px;
         width: 300px;
-        border: 2px solid #ffffff;
+        border: 2px solid red;
         border-radius: 4px;
-        font-style: normal;
-        font-weight: 300;
+        color: red;
+        outline: none;
         font-size: 14px;
-        text-align: center;
-        text-transform: uppercase;
-        color: #ffffff;
-        margin-bottom: 20px;
+        margin: 0 10px 10px 0px;
+        padding-left: 25px;
       }
       i {
         position: absolute;
-        left: 25px;
-        top: 15px;
+        left: 8px;
+        top: 13px;
         &::before {
-          color: #ffffff;
+          color: red;
         }
       }
     }
   }
   .form__block {
-    margin-top: 23px;
+    margin-top: 10px;
 
     &_btn {
       margin-bottom: 20px;
       width: 300px;
       height: 45px;
-      background: #ffffff;
+      background: transparent;
       box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
       border-radius: 4px;
       font-weight: 600;
       font-size: 16px;
-      text-align: center;
       text-transform: uppercase;
       color: #ff1141;
       transition: all 0.4s;
@@ -112,22 +211,27 @@ export default {
         background-color: #c02147;
         border: none;
         border: 1px solid #ffffff;
+        cursor: pointer;
         color: #ffffff;
         transform: scale(1.1);
       }
     }
     &_link {
-      text-align: center;
       font-weight: 500;
       font-size: 16px;
-      color: #ffffff;
+      color: red;
       &:hover {
         text-decoration: none;
       }
     }
   }
 }
-
+// register 
+.register-form{
+  display: flex;
+  width: 700px;
+  flex-wrap: wrap;
+}
 @media (max-width: 576px) {
   .login {
     &_block {
