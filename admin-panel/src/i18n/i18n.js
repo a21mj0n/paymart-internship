926a69636e05e5b70a3bba4417c5a4f2b5e54e07
azizbek <azizbekkarimjonov12@gmail.com>
2021-12-20 12:40:25 +0500

    users lang add

--- admin-panel/src/i18n/i18n.js
+++ admin-panel/src/i18n/i18n.js
@@ -4,7 +4,7 @@ import messages from './locales/index'
 import store from '../store'
 
 Vue.use(VueI18n)
-
+  
 const i18n = new VueI18n({
   locale: store.getters['lang/langChanged'],
   messages // установка сообщений локализаций
--- admin-panel/src/pages/admin/products/TestIndex.vue
+++ admin-panel/src/pages/admin/products/TestIndex.vue
@@ -10,7 +10,6 @@
       >
   <template slot="images" slot-scope="props">
           <div class="img__wrapper">
-            {{ }}
             <img v-if="props.rowData.image.length > 0" :src="`${configURL}/storage/product_images/${props.rowData.image[0].product_id}/` + props.rowData.image[0].name" alt="">
             <img v-else :src="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZWyg5k6Y2X4OaOfDMPcFaAwL9r_eN34CUXbEgCEjMepep7WMua2z90y_DGL0YobiBjRY&usqp=CAU'"  alt="">
           </div>
@@ -101,7 +100,6 @@ export default {
       page: 1,
       totalPages: "",
       fields: productFields(this.$i18n),
-
       configURL: config.URL.dev
     };
   },
