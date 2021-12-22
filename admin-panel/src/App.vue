2bce5bef3fc983d7a3d182214bfd40ce941bfbcf
azizbek <azizbekkarimjonov12@gmail.com>
2021-12-22 11:12:22 +0500

    container add

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -35,14 +35,14 @@ export default {
     margin-right: auto;
     padding-left: 15px;
     padding-right: 15px;
-    @media(min-width:1200px){
-      width: 1170px;
+    @media(min-width:768px){
+      width: 750px;
     }
     @media(min-width: 992px){
       width: 980px;
     }
-    @media(min-width:768px){
-      width: 750px;
+    @media(min-width:1200px){
+      width: 1170px;
     }
   }
    .test-wrapper{
